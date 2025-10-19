1a9538734e90f57f978b66a0ad4bb79102aad645
Adithyan <100783336+adithyanmkd@users.noreply.github.com>
2025-10-19 22:04:32 +0530

    Time: 0 ms (100%), Space: 58.7 MB (64.2%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    
+    function validate (root, min = -Infinity, max = Infinity) {
+        if (!root) return true
+
+        if (root.val <= min || root.val >= max) return false
+
+        return (
+            validate(root.left, min, root.val) &&
+            validate(root.right, root.val, max)
+        )
+    }
+
+    return validate(root)
+};
