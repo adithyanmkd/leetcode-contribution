3ab7cef5426b1bf6f60767ab742c8fe0b61c7efa
Adithyan <100783336+adithyanmkd@users.noreply.github.com>
2025-12-29 09:26:17 +0530

    Time: 0 ms (100%), Space: 53.8 MB (80.3%) - LeetHub

---
+++ 1185-day-of-the-week/1185-day-of-the-week.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function (day, month, year) {
+    const days = {
+        0: "Sunday", 1: "Monday", 2: "Tuesday", 
+        3: "Wednesday", 4: "Thursday", 5: "Friday", 
+        6: "Saturday"
+    }
+    const date = new Date(`${year}-${month}-${day}`)
+    
+    return days[date.getDay()]
+};
