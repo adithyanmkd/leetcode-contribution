4c0059e41cd068fcf1d6d9549841f05d570a4d12
Adithyan <100783336+adithyanmkd@users.noreply.github.com>
2026-01-31 23:10:14 +0530

    Time: 1 ms (17.16%), Space: 55.9 MB (27.45%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -0,0 +1,14 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+    for (let char of letters) {
+        if (char > target) {
+            return char
+        }
+    }
+
+    return letters[0]
+};
