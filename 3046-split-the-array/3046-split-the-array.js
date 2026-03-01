26b60a530f96b739800e75444782990feae41a10
Adithyan <100783336+adithyanmkd@users.noreply.github.com>
2026-03-01 22:19:20 +0530

    Time: 4 ms (23.36%), Space: 57.7 MB (27.1%) - LeetHub

---
+++ 3046-split-the-array/3046-split-the-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isPossibleToSplit = function(nums) {
+    const nums1 = [], nums2 = []
+    const n = nums.length
+
+    nums.sort()
+
+    for (let i = 0; i < n; i++) {
+        if (i % 2 === 0) nums1.push(nums[i])
+        else nums2.push(nums[i])
+    }
+
+    const set1 = new Set(nums1)
+    const set2 = new Set(nums2)
+
+    const ansN = nums.length / 2
+
+    // return set1.length === set2.length && ansN === set2.length && ansN === set1.length
+    const result = ansN === set1.size && ansN === set2.size
+    return result
+};
