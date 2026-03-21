fcd7bc2f4acb1ac41be7355a80f9260030e5d920
Adithyan <100783336+adithyanmkd@users.noreply.github.com>
2026-03-21 20:14:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -4,13 +4,10 @@
  */
 var diagonalSum = function(mat) {
     const n = mat.length
-    const pDiagonal = [] // primary diagonal
-    const sDiagonal = [] // secondary diagona
+    let sum = 0, posX = 0, posY = mat[0].length - 1
 
-    let posX = 0
-    let posY = mat[0].length - 1
     for (let i = 0; i < n; i++) {
-        pDiagonal.push(mat[i][posX])
+        sum += mat[i][posX]
 
         if (i === posX && i === posY) {
             posX++
@@ -18,11 +15,10 @@ var diagonalSum = function(mat) {
             continue
         }
 
-        sDiagonal.push(mat[i][posY])
+        sum += mat[i][posY]
         posX++
         posY--
     }
 
-    const sum = [...pDiagonal, ...sDiagonal].reduce((acc, val) => acc + val)
     return sum
 };
