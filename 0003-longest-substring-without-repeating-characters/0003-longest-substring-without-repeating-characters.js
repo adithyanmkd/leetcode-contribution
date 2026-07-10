e8ff6e88de1123c268510ba384eb5f8d07c2dc4a
Adithyan <100783336+adithyanmkd@users.noreply.github.com>
2026-07-10 12:37:05 +0530

    Time: 9 ms (38.14%), Space: 59.3 MB (39.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,28 +2,21 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function (s) {
-    if (s.length === 0 || s.length === 1) return s.length
-
+var lengthOfLongestSubstring = function(s) {
     const n = s.length
+    const charSet = new Set()
+    let res = 0
     let left = 0
-    let maxLen = 0
 
-    let set = new Set()
-
-    for (let right = 0; right < n; right++) {
-        if (set.has(s[right])) {
-            // If current char already exists in the set, we need to shrink the window
-            while (set.has(s[right])) {
-                // Remove characters from the left until the duplicate is removed
-                set.delete(s[left])
-                left++
-            }
+    for (let i = 0; i < n; i++) {
+        while (charSet.has(s[i])) {
+            charSet.delete(s[left])
+            left++
         }
 
-        set.add(s[right])
-        maxLen = Math.max(maxLen, set.size)
+        charSet.add(s[i])
+        res = Math.max(res, i - left + 1)
     }
 
-    return maxLen
+    return res
 };
